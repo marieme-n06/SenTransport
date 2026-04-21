89335678914ff84f03aef3ae62e74792c6e3fdec
Ndèye Marième Ndoye <riemamaty01@gmail.com>
2026-04-21 16:58:51 +0000

    Exercices Lab 1 : Footer, date Header, composant Statistique

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
+import Statistique1 from './Statistique1';
+import Statistique2 from './Statistique2';
+import Statistique3 from './Statistique3';
+
 
 function App() {
   return (
@@ -7,7 +12,13 @@ function App() {
       <Header  />
       <main className="contenu">
         <p>Bienvenue ! Cette application vous aide à trouver votre ligne de bus à Dakar.</p>
+        <div className="statistiques">
+          <Statistique1 />
+          <Statistique2 />
+          <Statistique3 />
+        </div>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/Statistique2.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique2() {
+  return (
+    <div className="statistique">
+      <span className="statistique-chiffre">100</span>
+      <span className="statistique-libelle">Arrêts</span>
+    </div>
+  );
+}
+
+export default Statistique2;

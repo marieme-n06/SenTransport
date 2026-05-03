8866639248ece56abf78ba813c8a450db65d2d7b
Ndèye Marième Ndoye <riemamaty01@gmail.com>
2026-05-03 21:03:40 +0000

    Lab 2 : composants statiques LigneBus et ListeLignes

---
+++ src/LigneBus.js
@@ -0,0 +1,17 @@
+import './LigneBus.css';
+function LigneBus({ numero, depart, arrivee, arrets, couleur }) {
+  return (
+    <div className="ligne-bus">
+      <div className="ligne-numero" style={{ backgroundColor: couleur }}>
+        {numero}
+      </div>
+      <div className="ligne-info">
+        <span className="ligne-trajet">
+          {depart} &rarr; {arrivee}
+        </span>
+        <span className="ligne-arrets">{arrets} arrêts</span>
+      </div>
+    </div>
+  );
+}
+export default LigneBus;
